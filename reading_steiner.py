ec66d6c2c14d97bd75d9eaf04acbaeda2ad68164
Swarup <swarupshakya63@gmail.com>
2026-09-20 07:54:03 +0545

    Parse git commit history

--- reading_steiner.py
+++ reading_steiner.py
@@ -10,6 +10,13 @@ for command in git_commands:
     result = subprocess.run(command, capture_output=True, text=True)
     results.append(result.stdout)
 
+for result in results:
+    wordResult = result.splitlines()
+
+for word in wordResult:
+    splitWord = word.split()
+    print(f"{splitWord[0]} -> {" ".join(splitWord[1:])}")
+
 print("-- READING STEINER --")
 
 print("Git gave us:")
