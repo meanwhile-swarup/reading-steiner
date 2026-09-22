e7a15b72a85ecbf95145f4b3a900feac19adec8a
Swarup <swarupshakya63@gmail.com>
2026-09-22 18:59:27 +0545

    feat: Total commits

--- reading_steiner.py
+++ reading_steiner.py
@@ -13,19 +13,23 @@ print("-- READING STEINER --\n")
 for title, command in git_commands:
     result = subprocess.run(command, capture_output=True, text=True)
     lines = result.stdout.splitlines()
-    
+
     print(f"=== {title} (Total lines: {len(lines)}) ===")
-    
+
+    if title == "Repository History":
+        print(f"Total commits: {len(lines)}")
+
     for line in lines:
         split_word = line.split()
+        
         if title == "Author":
             author_name = ' '.join(split_word[1:])
             print(f"Author is:{author_name}")
             continue
 
-        if split_word and len(split_word)>1:
+        if split_word and len(split_word) > 1:
             print(f"{split_word[0]} -> {' '.join(split_word[1:])}")
         else:
             print(" ".join(split_word))
             
-    print() 
+    print()
